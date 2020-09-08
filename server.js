c20de118b5c218dede5918a9377848ae32588024
Sean Beach <sean.beach1@yahoo.com>
2020-09-08 18:25:16 -0400

    db

---
+++ server.js
@@ -0,0 +1,61 @@
+
+const express = require('express')
+
+require('dotenv').config()
+
+const helmet = require('helmet')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+// db Connection w/ Heroku
+// const db = require('knex')({
+//   client: 'pg',
+//   connection: {
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: true,
+//   }
+// });
+
+// db Connection w/ localhost
+var db = require('knex')({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : '',
+    password : '',
+    database : 'tap-on-it'
+  }
+});
+
+// Controllers - aka, the db queries
+const main = require('./controllers/main')
+
+// App
+const app = express()
+
+// App Middleware
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(helmet())
+app.use(cors(corsOptions))
+app.use(bodyParser.json())
+app.use(morgan('combined'))
+
+app.get('/', (req, res) => res.send('hello world'))
+app.get('/crud', (req, res) => main.getTableData(req, res, db))
+app.post('/crud', (req, res) => main.postTableData(req, res, db))
+app.put('/crud', (req, res) => main.putTableData(req, res, db))
+app.delete('/crud', (req, res) => main.deleteTableData(req, res, db))
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT || 3000}`)
+})
